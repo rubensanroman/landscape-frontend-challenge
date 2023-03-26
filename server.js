558d014e2d08b6c5b8498e339771926a788968c7
Ruben Sanromán <ruben.sanroman@gmail.com>
2023-03-26 12:25:02 -0300

    Added server.js which validates form data

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const { body, validationResult } = require('express-validator');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+  })
+);
+
+app.use(
+  [
+    body('name').notEmpty(),
+    body('email').isEmail(),
+    body('phone').optional().isMobilePhone('es-ES'),
+    body('age').notEmpty().isInt({ min: 18 })
+  ],
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+
+    next();
+  }
+);
+
+app.post('/validate', (req, res) => {
+  const { name, email, phone, age } = req.body;
+  res.json({ mensaje: 'Datos enviados correctamente' });
+});
+
+const PORT = process.env.PORT || 3030;
+
+app.listen(PORT, () => {
+  console.log(`Servidor iniciado en puerto ${PORT}`);
+});
