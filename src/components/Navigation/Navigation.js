9d981efdb83045fdb77743619e8bcfdbb9cf61a9
Ruben Sanromán <ruben.sanroman@gmail.com>
2023-03-28 17:15:19 -0300

    Added responsive navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,17 +1,20 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 const Navigation = ({ currentPage }) => {
   return (
     <nav id="navigation">
       <ul>
-        <li className="navigationItem">
+        <li className={currentPage === 'home' ? 'active' : ''}>
           <Link href="/" className={currentPage === 'home' ? 'active' : ''}>
-            Formulario
+            <Image src={'/icon-form.png'} alt="Formulario de Registro" width={100} height={100} />
+            <p>Formulario</p>
           </Link>
         </li>
-        <li className="navigationItem">
-          <Link href="/pokemon" className={currentPage === 'pokemon' ? 'active' : ''}>
-            Pokemón
+        <li className={currentPage === 'pokemon' ? 'active' : ''}>
+          <Link href="/pokemon">
+            <Image src={'/icon-pokemon.png'} alt="Buscador de Pokemones" width={100} height={100} />
+            <p>Pokemón</p>
           </Link>
         </li>
       </ul>
